f011b5a5cddd0ff3b418e6bd53917f19f7d9fffe
bhavya <pulluribhavya782@gmail.com>
2026-09-21 19:18:58 +0530

    Temporarily enable debug for deployment error

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u*+ypfdb4d_$-in(-6isc@215a^qntb@e-!#gab%ahnnwtb!sg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
